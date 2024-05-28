c4597f08e969e9bfd924daa4dc7536d87a3f5d71
Nivaldo Junior <148484042+NivaldoGoncalves@users.noreply.github.com>
2024-05-28 19:07:27 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status")
-    expect(response.status).toBe(200);
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parseUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parseUpdateAt);
+
+  expect(responseBody.dependecies.database.version).toEqual("16.0");
+  expect(responseBody.dependecies.database.max_connections).toEqual(100);
+  expect(responseBody.dependecies.database.opened_connections).toEqual(1);
 });
