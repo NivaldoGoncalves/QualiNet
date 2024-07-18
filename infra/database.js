67a2f5d51610cd3cf704d9294bf4d800984cf171
Nivaldo Junior <148484042+NivaldoGoncalves@users.noreply.github.com>
2024-07-18 13:49:38 +0000

    adds `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,17 +1,11 @@
 import { Client } from "pg";
 
 async function query(queryOject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
+
+  let client;
 
   try {
-    await client.connect();
+    client = await getNewClient(); 
     const result = await client.query(queryOject);
     return result;
   } catch (error) {
@@ -25,3 +19,25 @@ async function query(queryOject) {
 export default {
   query: query,
 };
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+
+function getSSLValues() {
+  if (process.env.POSTEGRES_CA) {
+    return {
+      rejectUnauthorized: false,
+    };
+  }
+  return false;
+}
