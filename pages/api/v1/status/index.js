90ba29e05a4d1b21ffe298178115e29c5a8ea9ea
Nivaldo Junior <148484042+NivaldoGoncalves@users.noreply.github.com>
2024-09-11 11:53:31 +0000

    teste conexão

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "infra/database.js";
+/*import database from "infra/database.js";
 
 async function status(request, response) {
   const updateAt = new Date().toISOString();
@@ -19,6 +19,44 @@ async function status(request, response) {
 
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependecies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
+}
+
+export default status;*/
+import database from "infra/database.js";
+
+async function status(request, response) {
+  const updateAt = new Date().toISOString();
+
+  // Consulta para obter a versão do banco de dados Oracle
+  const databaseVersionResult = await database.query({
+    sql: "SELECT * FROM v$version WHERE banner LIKE 'Oracle%';",
+  });
+  const databaseVersionValue = databaseVersionResult.rows[0].BANNER;
+
+  // Consulta para obter o máximo de conexões permitidas no Oracle (limite de sessões)
+  const databaseMaxConnectionsResult = await database.query({
+    sql: `SELECT value FROM v$parameter WHERE name = 'sessions';`,
+  });
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].VALUE;
+
+  // Consulta para contar o número de conexões ativas
+  const databaseOpenedConnectionsResult = await database.query({
+    sql: "SELECT COUNT(*) AS count FROM v$session WHERE status = 'ACTIVE';",
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].COUNT;
 
   response.status(200).json({
     update_at: updateAt,
